e6684f72a19183d504dda224d3957300623a4e64
Logon Aluno <logonrmlocal@fiap.com.br>
2023-08-30 21:56:23 -0300

    checkpoint 01

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -10,11 +10,18 @@ terraform {
     }
   }
 
+  backend "s3" {
+    bucket         = "tf-staticsite-s3-state-v1"
+    key            = "terraform.tfstate"
+    dynamodb_table = "tf-staticsite-s3-state-v1"
+    region         = "us-east-1"
+  }
+
 }
 
 provider "aws" {
   region                   = "us-east-1"
-  shared_config_files      = ["./.aws/config"]
-  shared_credentials_files = ["./.aws/credentials"]
-  profile                  = "fiap"
+  # shared_config_files      = ["~/.aws/config"]
+  # shared_credentials_files = ["~/.aws/credentials"]
+  # profile                  = "fiap"
 }
